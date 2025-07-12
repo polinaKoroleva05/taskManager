340c5c82736796e0eefc6fc8dfe27a835a5c7845
Test User <polina.koroleva05@mail.ru>
2025-07-12 20:29:47 +0300

    add badge

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,11 +1,11 @@
-import { Button, Group, Paper, Text } from "@mantine/core"
+import { Badge, Button, Group, Paper, Text } from "@mantine/core"
 import type { TaskInterface } from "../types"
 import styles from '../css/taskItem.module.css'
 import LowPriorityIcon from '../assets/low.svg?react'
 import MediumPriorityIcon from '../assets/medium.svg?react'
 import HighPriorityIcon from '../assets/high.svg?react'
 import { useContext } from "react"
-import { Context } from "../Context"
+import { TasksContext } from "../Context"
 import { useNavigate } from "react-router"
 
 const IconMap = {
@@ -15,31 +15,24 @@ const IconMap = {
 }
 
 export default function TaskItem({ id }: { id: number }) {
-    const mapTasks: Map<number, TaskInterface> | null = useContext(Context)
+    const { tasks }: { tasks: TaskInterface[] } = useContext(TasksContext)
     let navigate = useNavigate()
-    let currentTask = mapTasks?.get(id)
+    let currentTask = tasks.find(item=>item.id === id)
     return (
         <Paper shadow='sm' radius='md' className={styles.taskItem}>
             {currentTask &&
                 <>
                     <Group justify="space-between">
-                        {IconMap[currentTask.priority]}
+                        <Badge color="blue" size="lg">{currentTask.priority}</Badge>
                         <Text fw={500}>
                             {currentTask.title}
                         </Text>
-                        <Button  variant="transparent" radius="md" onClick={()=>navigate(`task/${id}`)}><LowPriorityIcon className={styles.icon}/></Button>
-
-
-
+                        <Button variant="transparent" radius="md" onClick={() => navigate(`task/${id}`)}><LowPriorityIcon className={styles.icon} /></Button>
                     </Group>
                     {currentTask.description && <Text> {currentTask.description}</Text>}
                     <Group>
-                        <Text>
-                            {currentTask.category}
-                        </Text>
-                        <Text>
-                            {currentTask.status}
-                        </Text>
+                        <Badge color="blue" size="lg">{currentTask.category}</Badge>
+                        <Badge color="blue" size="lg">{currentTask.status}</Badge>
                     </Group>
                 </>
             }
